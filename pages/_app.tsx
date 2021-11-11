1ed8557194063f72179063c68729d3ca2db7d6c6
fx8kaleeb <kaleeb@null.net>
2021-11-11 11:03:39 -0500

    Changed font Mohave -> Lato

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ const theme = extendTheme({
 })
 
 const Everything = styled.span`
-  @import url('https://fonts.googleapis.com/css2?family=Mohave:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
 
   * {
     font-family: 'Mohave', sans-serif;
